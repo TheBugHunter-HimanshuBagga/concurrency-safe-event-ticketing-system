2b2028e9bb3c9af272f5ecac05d5e43821dbb631
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-04-06 22:39:07 +0530

    Event Entity

---
+++ event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/entity/Event.java
@@ -0,0 +1,34 @@
+package com.himanshu.event_ticketing_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime eventDateTime;
+
+    private int totalSeats;
+
+    private int availableSeats;
+}
