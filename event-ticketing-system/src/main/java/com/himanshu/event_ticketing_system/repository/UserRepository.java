3e532b3f99a74fc98c7760987a91b67d89ae2214
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-04-07 19:03:44 +0530

    added method for later Login purposes

--- event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/repository/UserRepository.java
+++ event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.himanshu.event_ticketing_system.repository;
 import com.himanshu.event_ticketing_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User , Long> {
+
+    Optional<User> findByEmail(String email); // optional helps handle the absence cases successfully
 
 }
