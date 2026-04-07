7432a731cb57c2879ebdbb4b3b91929aa602e4dc
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-04-07 20:58:33 +0530

    register User service code

--- event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/service/UserService.java
+++ event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/service/UserService.java
@@ -1,10 +1,38 @@
 package com.himanshu.event_ticketing_system.service;
 
+import com.himanshu.event_ticketing_system.dto.RegisterRequest;
+import com.himanshu.event_ticketing_system.dto.UserResponse;
+import com.himanshu.event_ticketing_system.entity.User;
+import com.himanshu.event_ticketing_system.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
 
+    public UserResponse registerUser(RegisterRequest request){
+        if(userRepository.findByEmail(request.email()).isPresent()){
+            throw new RuntimeException("User with this email already exists");
+        }
+        // if not present then create new user
+        User user = new User();
+        user.setName(request.name());
+        user.setEmail(request.email());
+        user.setPassword(request.password());
+        user.setRole(request.role());
+
+        User savedUser = userRepository.save(user);
+        return new UserResponse(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail()
+        );
+
+    }
 
 }
