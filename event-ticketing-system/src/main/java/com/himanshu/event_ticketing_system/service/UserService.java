0acd6627d57d4a3d0b1ffaea1e532f6b07440c97
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-04-07 19:10:55 +0530

    MYSQL: connected Successfully

---
+++ event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/service/UserService.java
@@ -0,0 +1,10 @@
+package com.himanshu.event_ticketing_system.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    /// TODO: TILL HERE USER SHOULD BE ABLE TO REFISTER SUCCESSFULLY
+
+
+}
