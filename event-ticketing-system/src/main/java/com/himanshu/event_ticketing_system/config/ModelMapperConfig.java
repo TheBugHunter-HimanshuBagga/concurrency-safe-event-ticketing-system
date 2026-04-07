9445f266ddfe864ab6b1bdd955a9bc74a2c533c5
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-04-07 21:16:58 +0530

    register User service code updated using ModelMapper

---
+++ event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.himanshu.event_ticketing_system.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
--- event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/service/UserService.java
+++ event-ticketing-system/src/main/java/com/himanshu/event_ticketing_system/service/UserService.java
@@ -5,6 +5,7 @@ import com.himanshu.event_ticketing_system.dto.UserResponse;
 import com.himanshu.event_ticketing_system.entity.User;
 import com.himanshu.event_ticketing_system.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -14,24 +15,31 @@ import java.util.Objects;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     public UserResponse registerUser(RegisterRequest request){
         if(userRepository.findByEmail(request.email()).isPresent()){
             throw new RuntimeException("User with this email already exists");
         }
         // if not present then create new user
-        User user = new User();
-        user.setName(request.name());
-        user.setEmail(request.email());
-        user.setPassword(request.password());
-        user.setRole(request.role());
 
+
+//        User user = new User();
+//        user.setName(request.name());
+//        user.setEmail(request.email());
+//        user.setPassword(request.password());
+//        user.setRole(request.role());
+//
+//        User savedUser = userRepository.save(user);
+//        return new UserResponse(
+//                savedUser.getId(),
+//                savedUser.getName(),
+//                savedUser.getEmail()
+//        );
+
+        User user = modelMapper.map(request , User.class);
         User savedUser = userRepository.save(user);
-        return new UserResponse(
-                savedUser.getId(),
-                savedUser.getName(),
-                savedUser.getEmail()
-        );
+        return modelMapper.map(savedUser , UserResponse.class);
 
     }
 
